5a9dd7aaf264d1eb914fd5d2e38df13bb1698405
laramuraveva <lara.chernishova@yandex.ru>
2022-07-28 15:25:53 +0300

    Files added

---
+++ src/main/java/ru/netology/ProductRepo.java
@@ -0,0 +1,47 @@
+package ru.netology;
+public class ProductRepo {
+
+    protected Product[] products = new Product[0];
+
+    public void add(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for(int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+
+    public Product[] findAll() {
+        return products;
+    }
+
+    public Product[] getProduct() {
+        return products;
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        if(findById(id) == null) {
+            throw new NotFoundException("Товара с id " + id + " нет.");
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for(Product product : products) {
+            if(product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        products = tmp;
+    }
+}
